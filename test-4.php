201b7586e839f298b595419ae21ebe3149cb0bf6
Steve Greig <greigsteve@gmail.com>
2018-02-03 18:50:58 +0000

    test-4.php still looking good

--- test-4.php
+++ test-4.php
@@ -1,12 +1,70 @@
 <?php
 
+function showForm($action, $form_array)	{
+	$form = "<form method='post' action=".$action.">";
+	foreach($form_array as $key => $value) {
+		$form .= "<p><label>".$form_array[$key]['form_label'];
+		if($form_array[$key]['type']=='select') {
+			$form .= " <select name='".$form_array[$key]['name']."'>";
+			foreach($form_array[$key]['options'] as $key_2 => $value_2) {
+				$form .= "<option value=".$key_2.">".$value_2."</option>";
+			}
+			$form .= "</select";
+		} else {
+			
+			/*
+			$form .= " <input name='".$form_array[$key]['name']
+				."' type='".$form_array[$key]['type']
+				."' value='".$form_array[$key]['value']
+				."'";
+			*/
+			
+			$form .= ' <input name="'.$form_array[$key]["name"]
+				.'" type="'.$form_array[$key]["type"]
+				.'" value="'.$form_array[$key]["value"]
+				.'"';
+				
+			if($form_array[$key]['required']=='required') {
+				$form .=" required";
+			}				
+		}
+	$form .=	"></label> ".$form_array[$key]['error_mssg']."</p>";
+	}
+	$form .= "<input type='submit'> <a href='index.php'> Cancel </a></form></br>";
+	return $form;
+}
+
+
+$snippet_form_array_0 = array ( 
+			"replicator" => array (
+						"name" => "replicator",
+						"required" => "",
+						"value" => "",
+						"error_mssg" => "",
+						"form_label" => "ask",
+						"type" => "text",
+						"validate" => ""
+						),
+);
+
+$snippet_form_array_1 = array ( 
+			"replicator" => array (
+						"name" => "replicator",
+						"required" => "",
+						"value" => "",
+						"error_mssg" => "",
+						"form_label" => "ask_2",
+						"type" => "text",
+						"validate" => ""
+						),
+);
+
+$action = 'index.php';
 $i = 1;
-$var_0 = 'tenn';
-$var_1 = 'eleven';
 $j = 0;
 while($j<$i+1)
 {
-	$x = 'var_'.$j;
-	echo $$x;
+	$x = 'snippet_form_array_'.$j;
+	echo showForm($action, $$x);
 	$j = $j + 1;
 }
